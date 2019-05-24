75f4ea2abdd73b9a126bf6b909d19276ea75edfa
nguyenhung98bk <48485798+nguyenhung98bk@users.noreply.github.com>
2019-05-24 20:55:10 +0700

    a

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php
-	header("Location: https://nguyenhung98bk.github.io/NghichTiThoi/?fbclid=IwAR0sIYyjh0vAU2K_d2BJxRhPSPoRUU2qV03cVmEmVlNCRAnvRBP9JZR3lPU");
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	abc
+</body>
+</html>
